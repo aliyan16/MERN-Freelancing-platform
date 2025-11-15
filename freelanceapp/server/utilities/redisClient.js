e5ae339ac1e88f0780db9dc1266b8885f0d4c304
aliyan16 <aliyanm12376@gmail.com>
2025-11-15 12:55:56 +0500

    deploying frontend

--- freelanceapp/server/utilities/redisClient.js
+++ freelanceapp/server/utilities/redisClient.js
@@ -1,18 +1,11 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: process.env.REDIS_URL
 });
 
-redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
-redisClient.on("connect", () => console.log("✅ Connected to Redis"));
-
-(async () => {
-  try {
-    await redisClient.connect();
-  } catch (err) {
-    console.error("❌ Redis connection failed:", err);
-  }
-})();
+redisClient.connect()
+  .then(() => console.log("Connected to Redis Cloud"))
+  .catch((err) => console.error("Redis Error:", err));
 
 export default redisClient;
